54087bf6c073b5b6fb5bdf7b0cf0b32d08385b3a
Stapaijio <mr.stepe2003@gmail.com>
2026-04-30 21:35:33 +0500

    add constants utility file

---
+++ src/utils/constants.py
@@ -0,0 +1,18 @@
+# Physical constants and default parameters
+import numpy as np
+
+# Constants
+G = 9.81  # Acceleration due to gravity [m/s^2]
+
+# Default parameters for a typical fluid (e.g., water-like or oil-like)
+DEFAULT_PARAMS = {
+    "rho": 1000.0,          # Density [kg/m^3]
+    "mu": 0.001,            # Dynamic viscosity [Pa*s] (water: 0.001, oil: 0.01-0.1)
+    "sigma": 0.072,         # Surface tension [N/m] (water: 0.072)
+    "h0": 0.001,            # Average film thickness [m] (1 mm)
+    "L": 5.0,               # Wall height [m]
+    "T": 10.0,              # Total observation time [s]
+    "delta": 1e-5,          # Initial perturbation amplitude [m]
+    "Nx": 1000,              # Number of spatial points
+    "dt": 1e-6,             # Time step [s] (Needs to be very small for stability)
+}
